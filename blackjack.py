65e1f2705efd7ea3b7d171b839bac696104d1e30
Subin Jo <soobeen0616@naver.com>
2023-07-11 14:54:31 +0900

    Edit multi play_game func

--- blackjack.py
+++ blackjack.py
@@ -126,14 +126,18 @@ def play_game():
         for player in players:
             if not player.player_end :
                 player_turn(player)
-
-        if not dealer.player_end :
-            dealer_turn(dealer)
-
+        
         all_player_end = all(player.player_end for player in players)
-        if all_player_end and dealer.player_end:
+        if all_player_end:
             break
         print("===============================================================")
+
+    print('\nDealer turn!!')
+    while True :
+        if not dealer.player_end :
+            dealer_turn(dealer)
+        else:
+            break
     
     # 딜러와 플레이어들의 total_score 출력
     print('\nTotal scores of the game')
